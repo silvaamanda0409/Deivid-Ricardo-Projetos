69fd041d315fe48116f00ef742d301fcdae19ae1
silvaamanda0409 <amanda.santos.silva0409@gmail.com>
2026-09-20 16:06:19 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,36 @@
+let list = document.querySelectorAll(".item")
+
+let next = document.getElementById("next")
+let prev = document.getElementById("prev")
+
+/*console.log(next)
+console.log(prev)
+console.log(list)
+*/
+
+let count = list.length
+console.log(count)
+let active = 0
+
+next.onclick = () => {
+
+let activeOld = document.querySelector(".active")
+activeOld.classList.remove("active")
+
+active = active >= count -1 ? 0: active + 1
+
+list[active].classList.add("active")
+
+}
+
+prev.onclick = () => {
+
+let activeOld = document.querySelector(".active")
+activeOld.classList.remove("active")
+
+active = active <= 0 ? count - 1 : active - 1
+list[active].classList.add("active")
+
+
+
+}
